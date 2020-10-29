e7b126c679843538925d6fe4e191a43e0432158d
しずえ <66309775+kuredoro@users.noreply.github.com>
2020-10-29 08:06:44 +0300

    Uncluttered main.go

---
+++ cmd/cptest/config.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+
+	"strconv"
+	"fmt"
+	"time"
+
+	"github.com/kureduro/cptest"
+)
+
+const defaultTL = 6 * time.Second
+
+func GetTL(inputs cptest.Inputs) (TL time.Duration) {
+    TL = defaultTL
+
+    if str, exists := inputs.Config["tl"]; exists {
+        sec, err := strconv.ParseFloat(str, 64)
+        if err != nil {
+            fmt.Printf("warning: time limit %q is of incorrect format: %v\n", str, err)
+            fmt.Printf("using default time limit: %v\n", defaultTL)
+            return
+        }
+
+        TL = time.Duration(sec * float64(time.Second))
+        return
+    }
+
+    fmt.Printf("using default time limit: %v\n", defaultTL)
+    return
+}
+
