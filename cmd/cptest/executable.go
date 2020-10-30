e3d0eb658aa5ee4b80c050baf711b19f537ce1e4
しずえ <cauchymoon@mail.ru>
2020-10-30 05:20:06 +0300

    Stopped checking that provided executable can be executed, because the fileinfo.Mode() doesn't work correctly on Windows. Executable now trims space line-wise.

--- cmd/cptest/executable.go
+++ cmd/cptest/executable.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"os/exec"
@@ -12,33 +11,38 @@ import (
 )
 
 type Executable struct {
-    Path string
+	Path string
+}
+
+func trimSpaceLineWise(text string) string {
+	var str strings.Builder
+	s := bufio.NewScanner(strings.NewReader(text))
+
+	for s.Scan() {
+		str.WriteString(strings.TrimSpace(s.Text()))
+		str.WriteRune('\n')
+	}
+
+	return str.String()
 }
 
 func (e *Executable) Run(r io.Reader, w io.Writer) error {
-    cmd := exec.Command(e.Path)
-    cmd.Stdin = r
+	cmd := exec.Command(e.Path)
+	cmd.Stdin = r
 
-    out, err := cmd.Output()
+	out, err := cmd.Output()
 
-    if ee, ok := err.(*exec.ExitError); ok {
-        cleanStderr := strings.TrimSpace(string(ee.Stderr))
-        fmt.Fprint(w, cleanStderr)
-        return fmt.Errorf("%v", ee)
-    }
+	if ee, ok := err.(*exec.ExitError); ok {
+		cleanStderr := trimSpaceLineWise(string(ee.Stderr))
+		fmt.Fprint(w, cleanStderr)
+		return fmt.Errorf("%v", ee)
+	}
 
-    if err != nil {
-        return fmt.Errorf("%w: %v", cptest.InternalErr, err)
-    }
+	if err != nil {
+		return fmt.Errorf("%w: %v", cptest.InternalErr, err)
+	}
 
-    var str strings.Builder
-    s := bufio.NewScanner(bytes.NewReader(out))
-
-    for s.Scan() {
-        str.WriteString(strings.TrimSpace(s.Text()))
-        str.WriteRune('\n')
-    }
-
-    fmt.Fprint(w, str.String())
-    return nil
+	cleanOut := trimSpaceLineWise(string(out))
+	fmt.Fprint(w, cleanOut)
+	return nil
 }
--- cmd/cptest/main.go
+++ cmd/cptest/main.go
@@ -14,9 +14,9 @@ var inputsPath string
 var execPath string
 
 func init() {
-    flag.Usage = func() {
-        fmt.Fprintf(flag.CommandLine.Output(),
-`CPTEST
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			`CPTEST
         Competitive programming write/test cycle automation tool.
 
 USAGE
@@ -24,10 +24,10 @@ USAGE
 
 FLAGS
 `)
-        flag.PrintDefaults()
+		flag.PrintDefaults()
 
-        fmt.Fprintf(flag.CommandLine.Output(),
-`
+		fmt.Fprintf(flag.CommandLine.Output(),
+			`
 EXECUTABLE SEARCHING
         If no executable path provided, cptest will try to find an executable
         inside working directory. If only one executable is found it is chosen
@@ -75,97 +75,99 @@ VERSION
         1.0.0
 
 `)
-    }
+	}
 
-    flag.StringVar(&inputsPath, "i", "inputs.txt", "File with the test cases (and, optionally, config)")
-    flag.StringVar(&execPath, "e", "", "Path to the executable")
+	flag.StringVar(&inputsPath, "i", "inputs.txt", "File with the test cases (and, optionally, config)")
+	flag.StringVar(&execPath, "e", "", "Path to the executable")
 }
 
 func GetProc() (proc cptest.Processer, err error) {
-    var path string
+	if execPath == "" {
+		execPath, err = FindExecutable(wd)
 
-    if execPath == "" {
-        path, err = FindExecutable(wd)
+		if err != nil {
+			return nil, err
+		}
 
-        if err != nil {
-            return nil, err
-        }
+		fmt.Printf("found executable: %s\n", joinIfRelative(wd, execPath))
+	}
 
-        fmt.Printf("found executable: %s\n", joinIfRelative(wd, path))
-    }
+	execPath = joinIfRelative(wd, execPath)
 
-    path = joinIfRelative(wd, path)
+	/*
+	   This check does not work on Windows.
+	   TODO: Find fix.
+	   if err = IsExec(execPath); err != nil {
+	       return nil, err
+	   }
+	*/
 
-    if err = IsExec(path); err != nil {
-        return nil, err
-    }
+	proc = &Executable{
+		Path: execPath,
+	}
 
-    proc = &Executable{
-        Path: path,
-    }
-
-    return
+	return
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    if count := len(flag.Args()); count != 0 {
-        wd = flag.Args()[0]
+	if count := len(flag.Args()); count != 0 {
+		wd = flag.Args()[0]
 
-        if count > 1 {
-            fmt.Printf("warning: expected 0 or 1 command line argument, got %v\n", count)
-        }
-    }
+		if count > 1 {
+			fmt.Printf("warning: expected 0 or 1 command line argument, got %v\n", count)
+		}
+	}
 
-    cwd, err := os.Getwd()
-    if err != nil {
-        fmt.Println("error: could not get path for current working directory")
-        return
-    }
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("error: could not get path for current working directory")
+		return
+	}
 
-    wd = joinIfRelative(cwd, wd)
-    if err = CheckWd(wd); err != nil {
-        fmt.Printf("error: %v\n", err)
-        return
-    }
+	wd = joinIfRelative(cwd, wd)
+	if err = CheckWd(wd); err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
 
-    inputsPath = joinIfRelative(wd, inputsPath)
+	inputsPath = joinIfRelative(wd, inputsPath)
 
-    inputs, errs := ReadInputs(inputsPath)
-    if errs != nil {
-        for _, err := range errs {
-            fmt.Printf("error: %v\n", err)
-        }
+	inputs, errs := ReadInputs(inputsPath)
+	if errs != nil {
+		for _, err := range errs {
+			fmt.Printf("error: %v\n", err)
+		}
 
-        return
-    }
+		return
+	}
 
-    proc, err := GetProc()
-    if err != nil {
-        fmt.Printf("error: %v\n", err)
-        return
-    }
+	proc, err := GetProc()
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
 
-    TL := GetTL(inputs)
-    swatch := cptest.NewConfigurableStopwatcher(TL)
+	TL := GetTL(inputs)
+	swatch := cptest.NewConfigurableStopwatcher(TL)
 
-    batch := cptest.NewTestingBatch(inputs, proc, swatch)
-    
-    batch.ResultPrinter = VerboseResultPrinter
+	batch := cptest.NewTestingBatch(inputs, proc, swatch)
 
-    batch.Run()
+	batch.ResultPrinter = VerboseResultPrinter
 
-    passCount := 0
-    for _, v := range batch.Stat {
-        if v == cptest.OK {
-            passCount++
-        }
-    }
+	batch.Run()
 
-    if passCount == len(batch.Stat) {
-        fmt.Println("OK")
-    } else {
-        fmt.Printf("FAIL\n%d/%d passed\n", passCount, len(batch.Stat))
-    }
+	passCount := 0
+	for _, v := range batch.Stat {
+		if v == cptest.OK {
+			passCount++
+		}
+	}
+
+	if passCount == len(batch.Stat) {
+		fmt.Println("OK")
+	} else {
+		fmt.Printf("FAIL\n%d/%d passed\n", passCount, len(batch.Stat))
+	}
 }
