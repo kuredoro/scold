1801b1fdc96698504368b7dec7adba99930afdbe
しずえ <cauchymoon@mail.ru>
2020-11-29 13:42:22 +0300

    Removed code duplication in main/executable: no line-wise space-trimming -> lexer does the job automatically.

--- cmd/cptest/executable.go
+++ cmd/cptest/executable.go
@@ -1,32 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"os/exec"
-	"strings"
 
 	"github.com/kuredoro/cptest"
 )
 
 type Executable struct {
 	Path string
-}
-
-func trimSpaceLineWise(text string) string {
-	var str strings.Builder
-	s := bufio.NewScanner(strings.NewReader(text))
-
-	for s.Scan() {
-        line := strings.TrimSpace(s.Text())
-        if line != "" {
-            str.WriteString(strings.TrimSpace(s.Text()))
-            str.WriteRune('\n')
-        }
-	}
-
-	return str.String()
 }
 
 func (e *Executable) Run(r io.Reader, w io.Writer) error {
@@ -36,8 +19,7 @@ func (e *Executable) Run(r io.Reader, w io.Writer) error {
 	out, err := cmd.Output()
 
 	if ee, ok := err.(*exec.ExitError); ok {
-		cleanStderr := trimSpaceLineWise(string(ee.Stderr))
-		fmt.Fprint(w, cleanStderr)
+		fmt.Fprint(w, string(ee.Stderr))
 		return fmt.Errorf("%v", ee)
 	}
 
@@ -45,7 +27,6 @@ func (e *Executable) Run(r io.Reader, w io.Writer) error {
 		return fmt.Errorf("%w: %v", cptest.InternalErr, err)
 	}
 
-	cleanOut := trimSpaceLineWise(string(out))
-	fmt.Fprint(w, cleanOut)
+	fmt.Fprint(w, string(out))
 	return nil
 }
