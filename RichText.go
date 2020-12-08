1aaf5560de68fb272b9af1ce0dd70dcb6f3ef79b
しずえ <cauchymoon@mail.ru>
2020-12-08 20:30:28 +0300

    Lexer.Compare: highlight text symbol-wise first test

--- RichText.go
+++ RichText.go
@@ -8,27 +8,27 @@ import (
 )
 
 type RichText struct {
-    Str string
-    Partition []int
+	Str       string
+	Partition []int
 }
 
 func (rt *RichText) Colorize(color aurora.Color) string {
-    var str strings.Builder
+	var str strings.Builder
 
-    sort.Ints(rt.Partition)
+	sort.Ints(rt.Partition)
 
-    start := 0
-    for i, end := range rt.Partition {
-        part := rt.Str[start:end]
+	start := 0
+	for i, end := range rt.Partition {
+		part := rt.Str[start:end]
 
-        if i & 1 == 0 {
-            str.WriteString(part)
-        } else {
-            str.WriteString(aurora.Colorize(part, color).String())
-        }
+		if i&1 == 0 {
+			str.WriteString(part)
+		} else {
+			str.WriteString(aurora.Colorize(part, color).String())
+		}
 
-        start = end
-    }
+		start = end
+	}
 
-    return str.String()
+	return str.String()
 }
