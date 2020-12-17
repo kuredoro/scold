be4b5a6c24aaf7f0c71cead8df2da9d789e51720
しずえ <66309775+kuredoro@users.noreply.github.com>
2020-12-17 21:36:40 +0300

    SpyProcesser is now concurrency safe.

--- processer.go
+++ processer.go
@@ -2,6 +2,7 @@ package cptest
 
 import (
 	"io"
+	"sync"
 )
 
 // Processer interface abstracts away the concept of the executable under
@@ -15,14 +16,25 @@ type Processer interface {
 type SpyProcesser struct {
 	Proc Processer
 
-	CallCount int
+    mu sync.Mutex
+	callCount int
 }
 
 // Run will execute the Run function of the inner processer, but will
 // also increase the call count by one.
 func (p *SpyProcesser) Run(r io.Reader, w io.Writer) error {
-	p.CallCount++
+    p.mu.Lock()
+	p.callCount++
+    p.mu.Unlock()
 	return p.Proc.Run(r, w)
+}
+
+// CallCount will return the number of times Run was called. Can be called
+// concurrently.
+func (p *SpyProcesser) CallCount() int {
+    p.mu.Lock()
+    defer p.mu.Unlock()
+	return p.callCount
 }
 
 // ProcesserFunc represents an implementation of Processer that instead
