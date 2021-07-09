17ced1f3083b7d033bdec63091e6faecda646a5a
しずえ <cauchymoon@mail.ru>
2021-07-09 12:50:05 +0300

    Emit absolute path in errors if no look up in PATH was anticipated.

--- cmd/cptest/filesystem.go
+++ cmd/cptest/filesystem.go
@@ -12,48 +12,55 @@ import (
 )
 
 func getPath() []string {
-    delim := ":"
-    if runtime.GOOS == "windows" {
-        delim = ";"
-    }
+	delim := ":"
+	if runtime.GOOS == "windows" {
+		delim = ";"
+	}
 
-    list := os.Getenv("PATH")
-    return strings.Split(list, delim)
+	list := os.Getenv("PATH")
+	return strings.Split(list, delim)
 }
 
 func findFile(userPath string) (string, error) {
-    // Ability to omit .exe prefix on Windows.
-    if runtime.GOOS == "windows" && filepath.Ext(userPath) == "" {
-        userPath += ".exe"
-    }
+	// Ability to omit .exe prefix on Windows.
+	if runtime.GOOS == "windows" && filepath.Ext(userPath) == "" {
+		userPath += ".exe"
+	}
 
-    absPath, err := filepath.Abs(userPath)
-    if err != nil {
-        fmt.Printf("warning: could not retrieve executables absolute path: %v. Will look in PATH", err)
-    }
+	absPath, err := filepath.Abs(userPath)
+	pathForError := absPath
+	if err != nil {
+		fmt.Printf("warning: could not retrieve executables absolute path: %v. Will look in PATH", err)
+		// For error report at the end of the func
+		pathForError = userPath
+	}
 
-    var candidates []string
-    if err == nil {
-        candidates = append(candidates, absPath)
-    }
+	var candidates []string
+	if err == nil {
+		candidates = append(candidates, absPath)
+	}
 
-    base := filepath.Base(userPath)
+	base := filepath.Base(userPath)
 
-    // If only the file name supplied, search in PATH
-    if filepath.Dir(userPath) == "." {
-        extraPaths := getPath()
-        for _, path := range extraPaths {
-            candidates = append(candidates, filepath.Join(path, base))
-        }
-    }
+	// If only the file name supplied, search in PATH
+	if filepath.Dir(userPath) == "." {
+		extraPaths := getPath()
+		for _, path := range extraPaths {
+			candidates = append(candidates, filepath.Join(path, base))
+		}
+	}
 
-    for _, cand := range candidates {
-        if _, err := os.Stat(cand); err == nil {
-            return cand, nil
-        }
-    }
+	for _, cand := range candidates {
+		if _, err := os.Stat(cand); err == nil {
+			return cand, nil
+		}
+	}
 
-    return "", fmt.Errorf("%s is absent in current working directory and in PATH", userPath)
+	if filepath.Dir(userPath) == "." {
+		return "", fmt.Errorf("%s is absent in current working directory and in PATH", userPath)
+	}
+
+	return "", fmt.Errorf("%s does not exist", pathForError)
 }
 
 func readInputs(inputsPath string) (cptest.Inputs, []error) {
