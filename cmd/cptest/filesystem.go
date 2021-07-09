0c44387ed98d9b74bc5e85d0dd529361017fe5f5
しずえ <cauchymoon@mail.ru>
2021-07-09 12:06:58 +0300

    Added findFile(string) that additionally looks for base of the path in PATH.

--- cmd/cptest/filesystem.go
+++ cmd/cptest/filesystem.go
@@ -4,9 +4,51 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	"github.com/kuredoro/cptest"
 )
+
+func getPath() []string {
+    delim := ":"
+    if runtime.GOOS == "windows" {
+        delim = ";"
+    }
+
+    list := os.Getenv("PATH")
+    return strings.Split(list, delim)
+}
+
+func findFile(userPath string) (string, error) {
+    absPath, err := filepath.Abs(userPath)
+    if err != nil {
+        fmt.Printf("warning: could not retrieve executables absolute path: %v. Will look in PATH", err)
+    }
+
+    var candidates []string
+    if err == nil {
+        candidates = append(candidates, absPath)
+    }
+
+    base := filepath.Base(userPath)
+
+    extraPaths := getPath()
+    for _, path := range extraPaths {
+        candidates = append(candidates, filepath.Join(path, base))
+    }
+
+    fmt.Printf("%#v\n", candidates)
+
+    for _, cand := range candidates {
+        if _, err := os.Stat(cand); err == nil {
+            return cand, nil
+        }
+    }
+
+    return "", fmt.Errorf("%s is absent in current working directory and in PATH", userPath)
+}
 
 func readInputs(inputsPath string) (cptest.Inputs, []error) {
 	inputsFile, err := os.Open(inputsPath)
