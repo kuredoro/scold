6fc931303e2bb53e969c8662f399281264bd0abb
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-07-16 21:02:20 +0300

    Single TL pass.

--- testing_batch.go
+++ testing_batch.go
@@ -27,6 +27,14 @@ const (
 	TL
 )
 
+type StringError string
+
+func (e StringError) Error() string {
+	return string(e)
+}
+
+const TLError StringError = "Time limit exceeded"
+
 // TestStartCallbackFunc represents a function to be called before a test
 // case will be launched. It accepts the id of the test case.
 type TestStartCallbackFunc func(int)
@@ -70,14 +78,15 @@ type TestingBatch struct {
 	RichAnswers map[int][]RichText
 	Lx          *Lexer
 
-	Verdicts map[int]Verdict
-	Times    map[int]time.Duration
+	Verdicts   map[int]Verdict
+	Times      map[int]time.Duration
+	startTimes map[int]time.Time
 
-	Proc   Processer
-    procCancels map[int]func()
-    procCancelsMu sync.Mutex
+	Proc          Processer
+	procCancels   map[int]func()
+	procCancelsMu sync.Mutex
 
-    ThreadPool WorkerPool
+	ThreadPool WorkerPool
 
 	Swatch Stopwatcher
 
@@ -106,13 +115,14 @@ func NewTestingBatch(inputs Inputs, proc Processer, swatch Stopwatcher, pool Wor
 			Precision: uint(precision),
 		},
 
-		Verdicts: make(map[int]Verdict),
-		Times:    make(map[int]time.Duration),
+		Verdicts:   make(map[int]Verdict),
+		Times:      make(map[int]time.Duration),
+		startTimes: make(map[int]time.Time),
 
-		Proc:   proc,
-        procCancels: make(map[int]func()),
+		Proc:        proc,
+		procCancels: make(map[int]func()),
 
-        ThreadPool: pool,
+		ThreadPool: pool,
 
 		Swatch: swatch,
 
@@ -132,21 +142,33 @@ func (b *TestingBatch) launchTest(id int, in string) {
 		}
 	}()
 
-    ctx, cancel := context.WithCancel(context.Background())
+    fmt.Printf("launchTest: id=%d\n", id)
+	ctx, cancel := context.WithCancel(context.Background())
 
-    b.procCancelsMu.Lock()
-    b.procCancels[id] = cancel
-    b.procCancelsMu.Unlock()
+	b.procCancelsMu.Lock()
+	b.procCancels[id] = cancel
+	b.procCancelsMu.Unlock()
 
 	out, err := b.Proc.Run(ctx, strings.NewReader(in))
 
-    fmt.Printf("Received %#v\n", out)
+	fmt.Printf("Received %#v, err=%v\n", out, err)
 
 	b.complete <- TestResult{
 		ID:  id,
 		Err: err,
 		Out: out,
 	}
+}
+
+func (b *TestingBatch) nextOldestRunning(previous int) int {
+	for id := previous + 1; id < len(b.inputs.Tests)+1; id++ {
+		_, finished := b.Verdicts[id]
+		if !finished {
+			return id
+		}
+	}
+
+	return len(b.inputs.Tests) + 1
 }
 
 // Run will lauch test cases in parallel and then will wait for each test to
@@ -156,75 +178,70 @@ func (b *TestingBatch) launchTest(id int, in string) {
 // each not-yet-judged test is assigned TL verdict and the ResultPrinter is
 // also called on each test.
 func (b *TestingBatch) Run() {
-    nextTestID := 0
-	for ; nextTestID < len(b.inputs.Tests); nextTestID += 1 {
-        id := nextTestID
-        err := b.ThreadPool.Execute(RunnableFunc(func(){
-            b.launchTest(id+1, b.inputs.Tests[id].Input)
-        }))
+	nextTestIndex := 0
+	for ; nextTestIndex < len(b.inputs.Tests); nextTestIndex += 1 {
+		id := nextTestIndex
+		err := b.ThreadPool.Execute(RunnableFunc(func() {
+			b.launchTest(id+1, b.inputs.Tests[id].Input)
+		}))
 
-        if err != nil {
-            break
-        }
+		if err != nil {
+			break
+		}
 
-		b.TestStartCallback(nextTestID + 1)
+		b.TestStartCallback(nextTestIndex + 1)
+		b.startTimes[id+1] = b.Swatch.Now()
 	}
 
+	oldestRunningID := 1
 	for range b.inputs.Tests {
 		var result TestResult
 
 		select {
-		case tl := <-b.Swatch.TimeLimit(b.Swatch.Now()):
-            tled := make([]int, 0, len(b.inputs.Tests))
-			for id := range b.inputs.Tests {
-				if _, finished := b.Verdicts[id+1]; !finished {
-                    tled = append(tled, id+1)
-				}
+		case tl := <-b.Swatch.TimeLimit(b.startTimes[oldestRunningID]):
+			id := oldestRunningID
+			b.Verdicts[id] = TL
+			b.Times[id] = tl.Sub(b.startTimes[id])
+
+			answerLexemes := b.Lx.Scan(b.inputs.Tests[id-1].Output)
+
+			rich := make([]RichText, len(answerLexemes))
+			for i, xm := range answerLexemes {
+				rich[i] = RichText{xm, make([]bool, len(xm))}
 			}
 
-            for _, id := range tled {
-                b.Verdicts[id] = TL
-                b.Times[id] = tl.Sub(b.Swatch.Now())
+			b.RichAnswers[id] = rich
 
-                answerLexemes := b.Lx.Scan(b.inputs.Tests[id-1].Output)
+			b.TestEndCallback(b, b.inputs.Tests[id-1], id)
 
-                rich := make([]RichText, len(answerLexemes))
-                for i, xm := range answerLexemes {
-                    rich[i] = RichText{xm, make([]bool, len(xm))}
-                }
+			oldestRunningID = b.nextOldestRunning(oldestRunningID)
 
-                b.RichAnswers[id] = rich
-
-                b.procCancelsMu.Lock()
-                if _, exists := b.procCancels[id]; exists {
-                    b.procCancels[id]()
-                }
-                b.procCancelsMu.Unlock()
-
-                b.TestEndCallback(b, b.inputs.Tests[id-1], id)
-            }
-
-            for range tled {
-                <-b.complete
-            }
-
-			return
+			b.procCancelsMu.Lock()
+			b.procCancels[id]()
+			b.procCancelsMu.Unlock()
+            continue
 		case result = <-b.complete:
 		}
 
-        fmt.Printf("b.complete %#v\n", result)
+		// TODO: REFACTOR remove tl branch
+		if result.Err == TLError {
+			continue
+		}
 
-        if nextTestID < len(b.inputs.Tests) {
-            id := nextTestID
-            err := b.ThreadPool.Execute(RunnableFunc(func(){
-                b.launchTest(id+1, b.inputs.Tests[id].Input)
-            }))
+		fmt.Printf("b.complete %#v\n", result)
 
-            if err == nil {
-                nextTestID++
-                b.TestStartCallback(nextTestID + 1)
-            }
-        }
+		if nextTestIndex < len(b.inputs.Tests) {
+			id := nextTestIndex+1
+			err := b.ThreadPool.Execute(RunnableFunc(func() {
+				b.launchTest(id, b.inputs.Tests[id-1].Input)
+			}))
+
+			if err == nil {
+				nextTestIndex++
+				b.TestStartCallback(id)
+                b.startTimes[id] = b.Swatch.Now()
+			}
+		}
 
 		id := result.ID
 		test := b.inputs.Tests[id-1]
@@ -257,5 +274,6 @@ func (b *TestingBatch) Run() {
 		}
 
 		b.TestEndCallback(b, test, id)
+		oldestRunningID = b.nextOldestRunning(oldestRunningID)
 	}
 }
