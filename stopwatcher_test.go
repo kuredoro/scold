11116ec27aed4b60301177680c38bae36cbb5feb
しずえ <cauchymoon@mail.ru>
2021-07-17 17:07:29 +0300

    SpyStopwatcher renamed to ConfigurableStopwatcher. Added basic test.

--- stopwatcher_test.go
+++ stopwatcher_test.go
@@ -1,130 +1,79 @@
 package cptest_test
 
-/*
 import (
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/jonboulle/clockwork"
 	"github.com/kuredoro/cptest"
 )
-*/
 
-/*
-func TestSpyStopwatcher(t *testing.T) {
-
-	t.Run("5 calls",
-		func(t *testing.T) {
-			const totalCalls = 5
-
-			swatch := &cptest.SpyStopwatcher{
-				TLAtCall: totalCalls,
-			}
-
-			for i := 0; i < totalCalls-1; i++ {
-				select {
-				case <-time.After(1 * time.Millisecond):
-					elapsedGot := swatch.Elapsed()
-					elapsedWant := time.Duration(i+1) * time.Second
-					if elapsedGot != elapsedWant {
-						t.Errorf("got %v elapsed, want %v", elapsedGot, elapsedWant)
-					}
-				case <-swatch.TimeLimit():
-					t.Errorf("got TL at call #%d, want at call #%d", i+1, totalCalls)
-				}
-			}
-
-			var firstTL, secondTL time.Duration
-			select {
-			case <-time.After(1 * time.Millisecond):
-				t.Fatalf("go no TL at call #%d, want one", totalCalls)
-			case firstTL = <-swatch.TimeLimit():
-			}
-
-			select {
-			case <-time.After(1 * time.Millisecond):
-				t.Fatalf("go no TL at call #%d, want one", totalCalls+1)
-			case secondTL = <-swatch.TimeLimit():
-			}
-
-			if firstTL != secondTL {
-				t.Errorf("got first and seconds TLs that don't match (%v != %v), want matching ones", firstTL, secondTL)
-			}
-
-			if firstTL != time.Duration(totalCalls)*time.Second {
-				t.Errorf("got TL equal to %v, want it equal to %v", firstTL, time.Duration(totalCalls))
-			}
-		})
-
-	t.Run("TL at 0 should never TL",
-		func(t *testing.T) {
-
-			swatch := &cptest.SpyStopwatcher{}
-
-			// There won't be more than 10 test cases in the tests, so I think it's
-			// enough
-			for i := 0; i < 10; i++ {
-				select {
-				case <-time.After(1 * time.Millisecond):
-				case <-swatch.TimeLimit():
-					t.Errorf("got TL at call #%d, want none", i+1)
-				}
-			}
-		})
-}
-*/
-
-/*
 func TestConfigurableStopwatcher(t *testing.T) {
 
-	t.Run("timeout after specified time",
+	t.Run("timeout after specified time given some initial time",
 		func(t *testing.T) {
+			TL := 5 * time.Second
+            clock := clockwork.NewFakeClock()
 
-			TL := 7 * time.Millisecond
-			timeStep := 2 * time.Millisecond
-			steps := int(TL / timeStep)
+            since := clock.Now().Add(5 * time.Second)
 
-			swatch := cptest.NewConfigurableStopwatcher(TL)
+            swatch := &cptest.ConfigurableStopwatcher{
+                TL: TL,
+                Clock: clock,
+            }
 
-			// It seems that this is enough of error for the check of the TL times
-			// at the end to work
-			const eps = 400 * time.Microsecond
+            done := make(chan time.Time)
 
-			for i := 0; i < steps; i++ {
-				elapsedWant := time.Duration(i+1) * timeStep
+            var wg sync.WaitGroup
+            wg.Add(1)
+            go func() {
+                t := <-swatch.TimeLimit(since)
+                done <- t
+            }()
 
+            clock.BlockUntil(1)
+
+            testStartTime := clock.Now()
+
+            var TLgot time.Time
+            var elapsedWant time.Duration
+			for i := 0; i < 20; i++ {
 				select {
-				case <-swatch.TimeLimit():
-					t.Errorf("got timelimit at %v, want at %v", elapsedWant, TL)
-				case <-time.After(timeStep):
-					elapsedGot := swatch.Elapsed()
+                case t := <-done:
+                    TLgot = t
+				case <-time.After(time.Millisecond):
+					elapsedGot := swatch.Elapsed(since)
 
-					// It seems that time.After is not precise enough. It's error
-					// seems to be ~0.2ms maximum (on my computer at least).
-					// Hence after consequent time.After in the case statement above
-					// error starts to accumulate and 'skew' the elapsedWant and
-					// elapsedGot by making the first one fall behind.
-					// This is my weak attempt to mitigate this.
-					skew := time.Duration(i+1) * eps
-
-					if elapsedGot-elapsedWant > skew {
-						t.Errorf("got elapsed %v, want %v", elapsedGot, elapsedWant)
+					if elapsedGot != elapsedWant {
+					    t.Errorf("at %v, got elapsed %v, want %v", clock.Now(), elapsedGot, elapsedWant)
 					}
+
+                    clock.Advance(1 * time.Second)
+                    if clock.Now().After(since) {
+                        elapsedWant += time.Second
+                    }
 				}
+
+                if TLgot != (time.Time{}) {
+                    break
+                }
+
 			}
 
-			time.Sleep(TL - time.Duration(steps)*timeStep)
+            TLwant := testStartTime.Add(10 * time.Second)
 
-			select {
-			case realTL := <-swatch.TimeLimit():
-				if realTL-TL > eps {
-					t.Errorf("received TL with value %v, it deviates from expected %v too much (%v)",
-						realTL, TL, realTL-TL)
-				}
-			case <-time.After(timeStep):
-				t.Errorf("got no timelimit at %v, want one", TL)
-			}
+            if TLgot == (time.Time{}) {
+                t.Fatalf("never received TL, want at %v\n", TLwant)
+            }
+
+            if TLgot != TLwant {
+                t.Fatalf("received TL at %v, want at %v\n", TLgot, TLwant)
+            }
+
 		})
 
+        /*
 	t.Run("don't timeout if TL less or equal 0",
 		func(t *testing.T) {
 			swatch := cptest.NewConfigurableStopwatcher(0)
@@ -139,5 +88,5 @@ func TestConfigurableStopwatcher(t *testing.T) {
 				}
 			}
 		})
+        */
 }
-*/
