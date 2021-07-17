1b09356282e6499b370e0b31d7a7831dd3640a27
しずえ <cauchymoon@mail.ru>
2021-07-17 19:29:22 +0300

    Updated 04 real example.

--- eg/04_1000_tests/gen.cpp
+++ eg/04_1000_tests/gen.cpp
@@ -9,13 +9,13 @@ int main()
     ofstream file("inputs.txt");
 
     mt19937 rd(time(0));
-    uniform_int_distribution<int64_t> dist(0, 1000100000);
+    uniform_int_distribution<int64_t> dist(0, 1000);
 
     for (int i = 0; i < 1000; i++)
     {
-        int64_t a = dist(rd), b = dist(rd);
-        file << a << ' ' << b << "\n---\n";
-        file << a + b << "\n===\n";
+        int64_t a = dist(rd);
+        file << a << "\n---\n";
+        file << a << "\n===\n";
     }
 
     return 0;
