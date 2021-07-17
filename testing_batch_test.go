1762d556d72b460bef999dc7f1396d095e6465be
しずえ <cauchymoon@mail.ru>
2021-07-17 11:26:19 +0300

    Added test with TL being run in parallel.

--- testing_batch_test.go
+++ testing_batch_test.go
@@ -544,6 +544,84 @@ func TestTestingBatch2(t *testing.T) {
 			cptest.AssertTimes(t, batch.Times, timesWant)
 		})
 
+	t.Run("two TL, thread count 2",
+		func(t *testing.T) {
+			inputs := cptest.Inputs{
+				Tests: []cptest.Test{
+					{"\n", "bar\n"},
+					{"\n", "bar\n"},
+				},
+			}
+
+			clock := clockwork.NewFakeClock()
+
+            var mu sync.Mutex
+			killCount := 0
+
+			proc := &cptest.SpyProcesser{
+				Proc: cptest.ProcesserFunc(
+					func(ctx context.Context, r io.Reader) (cptest.ProcessResult, error) {
+						select {
+						case <-clock.After(5 * time.Second):
+						case <-ctx.Done():
+                            mu.Lock()
+							killCount++
+                            mu.Unlock()
+							return cptest.ProcessResult{
+								ExitCode: 0,
+								Stdout:   "",
+								Stderr:   "",
+							}, cptest.TLError
+						}
+
+						return cptest.ProcessResult{
+							ExitCode: 0,
+							Stdout:   "",
+							Stderr:   "",
+						}, nil
+
+					}),
+			}
+
+			swatch := &cptest.SpyStopwatcher{
+				Clock: clock,
+				TL:    3 * time.Second,
+			}
+			pool := cptest.NewSpyThreadPool(2)
+
+			batch := cptest.NewTestingBatch(inputs, proc, swatch, pool)
+
+			var wg sync.WaitGroup
+			wg.Add(1)
+			go func() {
+				batch.Run()
+				wg.Done()
+			}()
+
+			clock.BlockUntil(3)
+			clock.Advance(3 * time.Second)
+
+			wg.Wait()
+
+			testsWant := map[int]cptest.Verdict{
+				1: cptest.TL,
+				2: cptest.TL,
+			}
+
+			timesWant := map[int]time.Duration{
+				1: 3 * time.Second,
+				2: 3 * time.Second,
+			}
+
+			cptest.AssertVerdicts(t, batch.Verdicts, testsWant)
+			cptest.AssertThreadCount(t, pool, 2)
+
+			cptest.AssertCallCount(t, "proc.Run()", proc.CallCount(), 2)
+			cptest.AssertCallCount(t, "process cancel", killCount, 2)
+			cptest.AssertTimes(t, batch.Times, timesWant)
+		})
+
+
 	t.Run("test cases may be abandoned at TL",
 		func(t *testing.T) {
 			return
