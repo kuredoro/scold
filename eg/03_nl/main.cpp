6244c74c4eed7f51d368c9361850ec669a72536b
しずえ <cauchymoon@mail.ru>
2021-07-18 18:39:19 +0300

    Altered 03 to allow for correct output if an cli argument was provided.

--- eg/03_nl/main.cpp
+++ eg/03_nl/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "\n5\n\n1 2 3 4 5\n\n";
+    if (argc != 1)
+        std::cout << "5\n1 2 3 4 5\n";
+    else
+        std::cout << "\n5\n\n1 2 3 4 5\n\n";
+
     return 0;
 }
