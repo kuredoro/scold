1b68988c72cca0403d1a726d2c2dbe094dc36f28
しずえ <supremeep@yandex.ru>
2021-09-18 16:48:20 +0300

    KVMapUnmarshal checks that the type is struct or a pointer to it.

--- kv_map_unmarshal.go
+++ kv_map_unmarshal.go
@@ -2,10 +2,28 @@ package cptest
 
 import (
     //"fmt"
+    "reflect"
+)
+
+const(
+    // NotStructLike is issued when a destination type doesn't behave like
+    // a struct. struct and *struct types have the same syntax for manipulating
+    // them, so they are considered struct-like.
+    NotAStructLike = StringError("not a struct-like")
 )
 
 type KVMap = map[string]string
 
 func KVMapUnmarshal(kvm KVMap, data interface{}) error {
+    val := reflect.ValueOf(data)
+
+    if val.Kind() == reflect.Ptr {
+        val = val.Elem()
+    }
+
+    if val.Kind() != reflect.Struct {
+        return NotAStructLike
+    }
+
     return nil
 }
