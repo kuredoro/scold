63fa2c1b369badf980b6abdd80dae214847db840
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-09-18 16:48:20 +0300

    KVMapUnmarshal checks that the type is struct or a pointer to it.

--- kv_map_unmarshal_test.go
+++ kv_map_unmarshal_test.go
@@ -37,6 +37,48 @@ func TestKVMapUnmarshal(t *testing.T) {
         td.Cmp(t, target, want)
     })
 
+    t.Run("unmarshal only works on structs or pointers to them", func(t *testing.T) {
+        kvm := map[string]string{}
+
+        err := cptest.KVMapUnmarshal(kvm, 42)
+        td.CmpError(t, err, cptest.NotAStructLike)
+
+        i := 42
+        err = cptest.KVMapUnmarshal(kvm, &i)
+        td.CmpError(t, err, cptest.NotAStructLike)
+
+        err = cptest.KVMapUnmarshal(kvm, "foo")
+        td.CmpError(t, err, cptest.NotAStructLike)
+
+        str := "foo"
+        err = cptest.KVMapUnmarshal(kvm, &str)
+        td.CmpError(t, err, cptest.NotAStructLike)
+
+        err = cptest.KVMapUnmarshal(kvm, []int{1, 2, 3})
+        td.CmpError(t, err, cptest.NotAStructLike)
+
+        err = cptest.KVMapUnmarshal(kvm, [...]int{1, 2, 3})
+        td.CmpError(t, err, cptest.NotAStructLike)
+
+        err = cptest.KVMapUnmarshal(kvm, kvm)
+        td.CmpError(t, err, cptest.NotAStructLike)
+
+        err = cptest.KVMapUnmarshal(kvm, func() {})
+        td.CmpError(t, err, cptest.NotAStructLike)
+
+        err = cptest.KVMapUnmarshal(kvm, make(chan int))
+        td.CmpError(t, err, cptest.NotAStructLike)
+
+        // ---
+
+        err = cptest.KVMapUnmarshal(kvm, struct{}{})
+        td.CmpNoError(t, err)
+
+        test := struct{}{}
+        err = cptest.KVMapUnmarshal(kvm, &test)
+        td.CmpNoError(t, err)
+    })
+
     // t.Run("int fields no error", func(t *testing.T) {
     //     type structType struct{
     //         untouched int
