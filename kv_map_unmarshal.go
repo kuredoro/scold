bdb1a9d800fc9337bb90b497e0ba000a0f8be8f1
しずえ <supremeep@yandex.ru>
2021-09-18 18:52:24 +0300

    KVMapUnmarshal returns errors on bogus values.

--- kv_map_unmarshal.go
+++ kv_map_unmarshal.go
@@ -32,6 +32,10 @@ func (e *NotValueOfType) Error() string {
     return fmt.Sprintf("value %q doesn't match %v type", e.Value, e.Type)
 }
 
+func (e *NotValueOfType) Equal(other *NotValueOfType) bool {
+    return e.Type == other.Type && e.Value == other.Value
+}
+
 type KVMap = map[string]string
 
 func KVMapUnmarshal(kvm KVMap, data interface{}) error {
@@ -57,38 +61,74 @@ func KVMapUnmarshal(kvm KVMap, data interface{}) error {
         }
 
         if field.Kind() == reflect.Int {
-            parsed, _ := strconv.Atoi(v)
-            // if err != nil {
-            //     errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
-            //     continue
-            // }
+            parsed, err := strconv.Atoi(v)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(parsed))
         } else if field.Kind() == reflect.Int8 {
-            parsed, _ := strconv.ParseInt(v, 10, 8)
+            parsed, err := strconv.ParseInt(v, 10, 8)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(int8(parsed)))
         } else if field.Kind() == reflect.Int16 {
-            parsed, _ := strconv.ParseInt(v, 10, 16)
+            parsed, err := strconv.ParseInt(v, 10, 16)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(int16(parsed)))
         } else if field.Kind() == reflect.Int32 {
-            parsed, _ := strconv.ParseInt(v, 10, 32)
+            parsed, err := strconv.ParseInt(v, 10, 32)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(int32(parsed)))
         } else if field.Kind() == reflect.Int64 {
-            parsed, _ := strconv.ParseInt(v, 10, 64)
+            parsed, err := strconv.ParseInt(v, 10, 64)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(int64(parsed)))
         } else if field.Kind() == reflect.Uint {
-            parsed, _ := strconv.ParseUint(v, 10, 0)
+            parsed, err := strconv.ParseUint(v, 10, 0)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(uint(parsed)))
         } else if field.Kind() == reflect.Uint8 {
-            parsed, _ := strconv.ParseUint(v, 10, 8)
+            parsed, err := strconv.ParseUint(v, 10, 8)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(uint8(parsed)))
         } else if field.Kind() == reflect.Uint16 {
-            parsed, _ := strconv.ParseUint(v, 10, 16)
+            parsed, err := strconv.ParseUint(v, 10, 16)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(uint16(parsed)))
         } else if field.Kind() == reflect.Uint32 {
-            parsed, _ := strconv.ParseUint(v, 10, 32)
+            parsed, err := strconv.ParseUint(v, 10, 32)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(uint32(parsed)))
         } else if field.Kind() == reflect.Uint64 {
-            parsed, _ := strconv.ParseUint(v, 10, 64)
+            parsed, err := strconv.ParseUint(v, 10, 64)
+            if err != nil {
+                errs = multierror.Append(errs, &NotValueOfType{field.Kind(), v})
+                continue
+            }
             field.Set(reflect.ValueOf(uint64(parsed)))
         } else {
             // ...
