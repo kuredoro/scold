12d5b59bbaba8504a5d96b3372e76a224b4ef0f8
しずえ <supremeep@yandex.ru>
2021-09-28 02:09:21 +0300

    Removed Header field from LineError. Added TestError.

--- common.go
+++ common.go
@@ -1,7 +1,7 @@
 package cptest
 
 import (
-    "fmt"
+	"fmt"
 )
 
 // StringError is an error type whose values can be constant and compared against deterministically with == operator. An error type that solves the problems of sentinel errors.
@@ -14,16 +14,27 @@ func (e StringError) Error() string {
 // LineError appends line information to the error message. It is mainly used
 // to test that errors are produced for correct lines.
 type LineError struct {
-	Header string
-	Line   int
-	Err    error
+	Line int
+	Err  error
 }
 
 func (e *LineError) Error() string {
-	return fmt.Sprintf("%s: line %d: %v", e.Header, e.Line, e.Err)
+	return fmt.Sprintf("line %d: %v", e.Line, e.Err)
 }
 
 func (e *LineError) Unwrap() error {
 	return e.Err
 }
 
+type TestError struct {
+	TestNum int
+	Err     error
+}
+
+func (e *TestError) Error() string {
+	return fmt.Sprintf("test %d: %v", e.TestNum, e.Err)
+}
+
+func (e *TestError) Unwrap() error {
+	return e.Err
+}
