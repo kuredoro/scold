394d3d0ca26f81a17e3a4c2ee75cfc1e771484db
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-09-29 02:16:25 +0300

    LineRangeError contains an array of Lines now. Pretty prints them by default.

--- common.go
+++ common.go
@@ -2,6 +2,7 @@ package cptest
 
 import (
 	"fmt"
+	"strings"
 )
 
 // StringError is an error type whose values can be constant and compared against deterministically with == operator. An error type that solves the problems of sentinel errors.
@@ -15,16 +16,39 @@ func (e StringError) Error() string {
 // to test that errors are produced for correct lines.
 type LineRangeError struct {
 	Begin int
-	End   int
+	Lines []string
 	Err   error
 }
 
 func (e *LineRangeError) Error() string {
-	if e.Begin+1 == e.End {
+	if len(e.Lines) == 0 {
 		return fmt.Sprintf("line %d: %v", e.Begin, e.Err)
 	}
 
-	return fmt.Sprintf("lines %d-%d: %v", e.Begin, e.End-1, e.Err)
+	var msg strings.Builder
+    if len(e.Lines) == 1 {
+        msg.WriteString(fmt.Sprintf("line %d: %v\n", e.Begin, e.Err))
+    } else {
+        msg.WriteString(fmt.Sprintf("lines %d-%d: %v\n", e.Begin, e.Begin + len(e.Lines), e.Err))
+    }
+
+    msg.WriteString(e.CodeSnippet())
+
+	return msg.String()
+}
+
+func (e *LineRangeError) CodeSnippet() string {
+    var msg strings.Builder
+
+	for i, line := range e.Lines {
+		if line != "" && line[len(line)-1] == '\n' {
+			line = line[:len(line)-1]
+		}
+
+		msg.WriteString(fmt.Sprintf("% 6d| %s\n", e.Begin+i, line))
+	}
+
+    return msg.String()
 }
 
 func (e *LineRangeError) Unwrap() error {
